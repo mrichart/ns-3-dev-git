26b3c21cb7558f2172116a574b6deac7ff3d8276
Matias Richart <mrichart@fing.edu.uy>
2015-06-12 20:06:58 -0300

    Add Rrpaa to examples to run

--- examples/wireless/examples-to-run.py
+++ examples/wireless/examples-to-run.py
@@ -27,6 +27,7 @@ cpp_examples = [
     ("wifi-wired-bridging", "True", "True"),
     ("power-adaptation-distance --manager=ns3::ParfWifiManager --outputFileName=parf --steps=5 --stepsSize=10", "True", "True"),
     ("power-adaptation-distance --manager=ns3::AparfWifiManager --outputFileName=aparf --steps=5 --stepsSize=10", "True", "True"),
+    ("power-adaptation-distance --manager=ns3::RrpaaWifiManager --outputFileName=rrpaa --steps=5 --stepsSize=10", "True", "True"),
 ]
 
 # A list of Python examples to run in order to ensure that they remain
