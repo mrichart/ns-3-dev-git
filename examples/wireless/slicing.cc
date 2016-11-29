4f80704b9887c987e468e9b53e93d66a7060a61d
Matias <mrichart@fing.edu.uy>
2016-11-29 12:34:31 -0300

    Add more stations to test

--- examples/wireless/slicing.cc
+++ examples/wireless/slicing.cc
@@ -124,7 +124,7 @@ NodeStatistics::AdvancePosition (Ptr<Node> node, int stepsSize, int stepsTime)
   Vector pos = GetPosition (node);
   double mbs = ((m_bytesTotal * 8.0) / (1000000 * stepsTime));
   m_bytesTotal = 0;
-  m_output.Add (pos.x, mbs);
+  m_output.Add ((Simulator::Now ()).GetSeconds (), mbs);
   pos.x += stepsSize;
   SetPosition (node, pos);
   Simulator::Schedule (Seconds (stepsTime), &NodeStatistics::AdvancePosition, this, node, stepsSize, stepsTime);
@@ -158,6 +158,12 @@ int main (int argc, char *argv[])
   int sta1_y = 0;
   int sta2_x = -5;
   int sta2_y = 0;
+  int sta3_x = 0;
+  int sta3_y = 5;
+  int sta4_x = 0;
+  int sta4_y = -5;
+  double sta5_x = 3.5;
+  double sta5_y = 3.5;
   int steps = 100;
   int stepsSize = 1;
   int stepsTime = 1;
@@ -178,8 +184,14 @@ int main (int argc, char *argv[])
   cmd.AddValue ("AP1_y", "Position of AP1 in y coordinate", ap1_y);
   cmd.AddValue ("STA1_x", "Position of STA1 in x coordinate", sta1_x);
   cmd.AddValue ("STA1_y", "Position of STA1 in y coordinate", sta1_y);
-  cmd.AddValue ("STA2_x", "Position of STA1 in x coordinate", sta2_x);
-  cmd.AddValue ("STA2_y", "Position of STA1 in y coordinate", sta2_y);
+  cmd.AddValue ("STA2_x", "Position of STA2 in x coordinate", sta2_x);
+  cmd.AddValue ("STA2_y", "Position of STA2 in y coordinate", sta2_y);
+  cmd.AddValue ("STA3_x", "Position of STA3 in x coordinate", sta3_x);
+  cmd.AddValue ("STA3_y", "Position of STA3 in y coordinate", sta3_y);
+  cmd.AddValue ("STA4_x", "Position of STA4 in x coordinate", sta4_x);
+  cmd.AddValue ("STA4_y", "Position of STA4 in y coordinate", sta4_y);
+  cmd.AddValue ("STA5_x", "Position of STA5 in x coordinate", sta5_x);
+  cmd.AddValue ("STA5_y", "Position of STA5 in y coordinate", sta5_y);
   cmd.Parse (argc, argv);
 
   int simuTime = steps * stepsTime;
@@ -190,7 +202,7 @@ int main (int argc, char *argv[])
 
   //Define the STAs
   NodeContainer wifiStaNodes;
-  wifiStaNodes.Create (2);
+  wifiStaNodes.Create (5);
 
 
   YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
@@ -304,6 +316,9 @@ int main (int argc, char *argv[])
   positionAlloc->Add (Vector (ap1_x, ap1_y, 0.0));
   positionAlloc->Add (Vector (sta1_x, sta1_y, 0.0));
   positionAlloc->Add (Vector (sta2_x, sta2_y, 0.0));
+  positionAlloc->Add (Vector (sta3_x, sta3_y, 0.0));
+  positionAlloc->Add (Vector (sta4_x, sta4_y, 0.0));
+  positionAlloc->Add (Vector (sta5_x, sta5_y, 0.0));
   mobility.SetPositionAllocator (positionAlloc);
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (wifiApNodes.Get (0));
@@ -312,10 +327,16 @@ int main (int argc, char *argv[])
   //Statistics counter
   NodeStatistics atpCounter = NodeStatistics (wifiApDevices, wifiStaDevices);
   NodeStatistics atpCounter2 = NodeStatistics (wifiApDevices, wifiStaDevices);
+  NodeStatistics atpCounter3 = NodeStatistics (wifiApDevices, wifiStaDevices);
+  NodeStatistics atpCounter4 = NodeStatistics (wifiApDevices, wifiStaDevices);
+  NodeStatistics atpCounter5 = NodeStatistics (wifiApDevices, wifiStaDevices);
 
   //Move the STA by stepsSize meters every stepsTime seconds
   Simulator::Schedule (Seconds (0.5 + stepsTime), &NodeStatistics::AdvancePosition, &atpCounter, wifiStaNodes.Get (0), stepsSize, stepsTime);
   Simulator::Schedule (Seconds (0.5 + stepsTime), &NodeStatistics::AdvancePosition, &atpCounter2, wifiStaNodes.Get (1), stepsSize, stepsTime);
+  Simulator::Schedule (Seconds (0.5 + stepsTime), &NodeStatistics::AdvancePosition, &atpCounter3, wifiStaNodes.Get (2), stepsSize, stepsTime);
+  Simulator::Schedule (Seconds (0.5 + stepsTime), &NodeStatistics::AdvancePosition, &atpCounter4, wifiStaNodes.Get (3), stepsSize, stepsTime);
+  Simulator::Schedule (Seconds (0.5 + stepsTime), &NodeStatistics::AdvancePosition, &atpCounter5, wifiStaNodes.Get (4), stepsSize, stepsTime);
 
   //Configure the IP stack
   InternetStackHelper stack;
@@ -326,6 +347,9 @@ int main (int argc, char *argv[])
   Ipv4InterfaceContainer i = address.Assign (wifiDevices);
   Ipv4Address sinkAddress = i.GetAddress (0);
   Ipv4Address sinkAddress2 = i.GetAddress (1);
+  Ipv4Address sinkAddress3 = i.GetAddress (2);
+  Ipv4Address sinkAddress4 = i.GetAddress (3);
+  Ipv4Address sinkAddress5 = i.GetAddress (4);
   uint16_t port = 9;
 
   //Configure the CBR generator
@@ -333,18 +357,42 @@ int main (int argc, char *argv[])
   ApplicationContainer apps_sink = sink.Install (wifiStaNodes.Get (0));
   PacketSinkHelper sink2 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress2, port));
   apps_sink.Add(sink2.Install (wifiStaNodes.Get (1)));
+  PacketSinkHelper sink3 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress3, port));
+  apps_sink.Add(sink3.Install (wifiStaNodes.Get (2)));
+  PacketSinkHelper sink4 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress4, port));
+  apps_sink.Add(sink4.Install (wifiStaNodes.Get (3)));
+  PacketSinkHelper sink5 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress5, port));
+  apps_sink.Add(sink5.Install (wifiStaNodes.Get (4)));
 
   OnOffHelper onoff ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress, port));
-  onoff.SetConstantRate (DataRate ("100Mb/s"), 1420);
+  onoff.SetConstantRate (DataRate ("20Mb/s"), 1420);
   onoff.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
   onoff.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   ApplicationContainer apps_source = onoff.Install (wifiApNodes.Get (0));
 
   OnOffHelper onoff2 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress2, port));
-  onoff2.SetConstantRate (DataRate ("100Mb/s"), 1420);
+  onoff2.SetConstantRate (DataRate ("20Mb/s"), 1420);
   onoff2.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
   onoff2.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff2.Install (wifiApNodes.Get (0)));
+
+  OnOffHelper onoff3 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress3, port));
+  onoff3.SetConstantRate (DataRate ("20Mb/s"), 1420);
+  onoff3.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff3.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  apps_source.Add(onoff3.Install (wifiApNodes.Get (0)));
+
+  OnOffHelper onoff4 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress4, port));
+  onoff4.SetConstantRate (DataRate ("20Mb/s"), 1420);
+  onoff4.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff4.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  apps_source.Add(onoff4.Install (wifiApNodes.Get (0)));
+
+  OnOffHelper onoff5 ("ns3::UdpSocketFactory", InetSocketAddress (sinkAddress5, port));
+  onoff5.SetConstantRate (DataRate ("20Mb/s"), 1420);
+  onoff5.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff5.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  apps_source.Add(onoff5.Install (wifiApNodes.Get (0)));
 
   apps_sink.Start (Seconds (0.5));
   apps_sink.Stop (Seconds (simuTime));
@@ -358,6 +406,12 @@ int main (int argc, char *argv[])
                    MakeCallback (&NodeStatistics::RxCallback, &atpCounter));
   Config::Connect ("/NodeList/2/ApplicationList/*/$ns3::PacketSink/Rx",
                    MakeCallback (&NodeStatistics::RxCallback, &atpCounter2));
+  Config::Connect ("/NodeList/3/ApplicationList/*/$ns3::PacketSink/Rx",
+                   MakeCallback (&NodeStatistics::RxCallback, &atpCounter3));
+  Config::Connect ("/NodeList/4/ApplicationList/*/$ns3::PacketSink/Rx",
+                   MakeCallback (&NodeStatistics::RxCallback, &atpCounter4));
+  Config::Connect ("/NodeList/5/ApplicationList/*/$ns3::PacketSink/Rx",
+                   MakeCallback (&NodeStatistics::RxCallback, &atpCounter5));
 
   //Callbacks to print every change of rate
   Config::Connect ("/NodeList/0/DeviceList/*/$ns3::WifiNetDevice/RemoteStationManager/$" + apManager + "/RateChange",
@@ -381,6 +435,30 @@ int main (int argc, char *argv[])
   gnuplot.SetTitle ("Throughput (AP to STA) vs time");
   gnuplot.AddDataset (atpCounter2.GetDatafile ());
   gnuplot.GenerateOutput (outfile2);
+
+  std::ofstream outfile3 (("throughput-" + outputFileName + "3.plt").c_str ());
+  gnuplot = Gnuplot (("throughput-" + outputFileName + "3.eps").c_str (), "Throughput");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Throughput (Mb/s)");
+  gnuplot.SetTitle ("Throughput (AP to STA) vs time");
+  gnuplot.AddDataset (atpCounter3.GetDatafile ());
+  gnuplot.GenerateOutput (outfile3);
+
+  std::ofstream outfile4 (("throughput-" + outputFileName + "4.plt").c_str ());
+  gnuplot = Gnuplot (("throughput-" + outputFileName + "4.eps").c_str (), "Throughput");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Throughput (Mb/s)");
+  gnuplot.SetTitle ("Throughput (AP to STA) vs time");
+  gnuplot.AddDataset (atpCounter4.GetDatafile ());
+  gnuplot.GenerateOutput (outfile4);
+
+  std::ofstream outfile5 (("throughput-" + outputFileName + "5.plt").c_str ());
+  gnuplot = Gnuplot (("throughput-" + outputFileName + "5.eps").c_str (), "Throughput");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Throughput (Mb/s)");
+  gnuplot.SetTitle ("Throughput (AP to STA) vs time");
+  gnuplot.AddDataset (atpCounter5.GetDatafile ());
+  gnuplot.GenerateOutput (outfile5);
 
   Simulator::Destroy ();
 
