3e7c8e67a682dc1efd4b8aadb382bc363f776cd8
Matías Richart <mrichart@fing.edu.uy>
2018-11-22 15:14:27 -0300

    Add channel statistics and parametrize slices and interference

--- examples/wireless/slicing-many-stations-interference.cc
+++ examples/wireless/slicing-many-stations-interference.cc
@@ -62,6 +62,158 @@ using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("SlicingManyStations");
 
+class NodeStatistics
+{
+public:
+  NodeStatistics (NetDeviceContainer aps, NetDeviceContainer stas);
+
+  void CheckStatistics (double time);
+
+  void RxCallback (std::string path, Ptr<const Packet> packet, const Address &from);
+  void StateCallback (std::string path, Time init, Time duration, enum WifiPhy::State state);
+
+  Gnuplot2dDataset GetDatafile ();
+  Gnuplot2dDataset GetPowerDatafile ();
+  Gnuplot2dDataset GetIdleDatafile ();
+  Gnuplot2dDataset GetBusyDatafile ();
+  Gnuplot2dDataset GetTxDatafile ();
+  Gnuplot2dDataset GetRxDatafile ();
+
+  double GetBusyTime ();
+
+private:
+  typedef std::vector<std::pair<Time,WifiMode> > TxTime;
+  void SetupPhy (Ptr<WifiPhy> phy);
+  Time GetCalcTxTime (WifiMode mode);
+
+  uint32_t m_bytesTotal;
+
+  double totalTime;
+  double busyTime;
+  double idleTime;
+  double txTime;
+  double rxTime;
+
+  double totalBusyTime;
+  double totalIdleTime;
+  double totalTxTime;
+  double totalRxTime;
+
+  Ptr<WifiPhy> myPhy;
+
+  Gnuplot2dDataset m_output;
+  Gnuplot2dDataset m_output_power;
+  Gnuplot2dDataset m_output_idle;
+  Gnuplot2dDataset m_output_busy;
+  Gnuplot2dDataset m_output_rx;
+  Gnuplot2dDataset m_output_tx;
+};
+
+NodeStatistics::NodeStatistics (NetDeviceContainer aps, NetDeviceContainer stas)
+{
+  totalTime = 0;
+
+  busyTime = 0;
+  idleTime = 0;
+  txTime = 0;
+  rxTime = 0;
+
+  totalBusyTime = 0;
+  totalIdleTime = 0;
+  totalTxTime = 0;
+  totalRxTime = 0;
+
+  m_bytesTotal = 0;
+
+  m_output.SetTitle ("Throughput Mbits/s");
+  m_output_idle.SetTitle ("Idle Time");
+  m_output_busy.SetTitle ("Busy Time");
+  m_output_rx.SetTitle ("RX Time");
+  m_output_tx.SetTitle ("TX Time");
+}
+
+void
+NodeStatistics::StateCallback (std::string path, Time init, Time duration, enum WifiPhy::State state)
+{
+  if (state == WifiPhy::CCA_BUSY)
+    {
+      busyTime += duration.GetSeconds ();
+      totalBusyTime += duration.GetSeconds ();
+    }
+  else if (state == WifiPhy::IDLE)
+    {
+      idleTime += duration.GetSeconds ();
+      totalIdleTime += duration.GetSeconds ();
+    }
+  else if (state == WifiPhy::TX)
+    {
+      txTime += duration.GetSeconds ();
+      totalTxTime += duration.GetSeconds ();
+    }
+  else if (state == WifiPhy::RX)
+    {
+      rxTime += duration.GetSeconds ();
+      totalRxTime += duration.GetSeconds ();
+    }
+}
+
+void
+NodeStatistics::RxCallback (std::string path, Ptr<const Packet> packet, const Address &from)
+{
+  m_bytesTotal += packet->GetSize ();
+}
+
+void
+NodeStatistics::CheckStatistics (double time)
+{
+  double mbs = ((m_bytesTotal * 8.0) / (1000000 * time));
+  m_bytesTotal = 0;
+  totalTime = 0;
+  m_output.Add ((Simulator::Now ()).GetSeconds (), mbs);
+
+  m_output_idle.Add ((Simulator::Now ()).GetSeconds (), idleTime * 100);
+  m_output_busy.Add ((Simulator::Now ()).GetSeconds (), busyTime * 100);
+  m_output_tx.Add ((Simulator::Now ()).GetSeconds (), txTime * 100);
+  m_output_rx.Add ((Simulator::Now ()).GetSeconds (), rxTime * 100);
+
+  busyTime = 0;
+  idleTime = 0;
+  txTime = 0;
+  rxTime = 0;
+
+  Simulator::Schedule (Seconds (time), &NodeStatistics::CheckStatistics, this, time);
+}
+
+Gnuplot2dDataset
+NodeStatistics::GetDatafile ()
+{
+  return m_output;
+}
+
+Gnuplot2dDataset
+NodeStatistics::GetIdleDatafile ()
+{
+  return m_output_idle;
+}
+
+Gnuplot2dDataset
+NodeStatistics::GetBusyDatafile ()
+{
+  return m_output_busy;
+}
+
+Gnuplot2dDataset
+NodeStatistics::GetRxDatafile ()
+{
+  return m_output_rx;
+}
+
+Gnuplot2dDataset
+NodeStatistics::GetTxDatafile ()
+{
+  return m_output_tx;
+}
+
 int main (int argc, char *argv[])
 {
   uint32_t rtsThreshold = 65535;
@@ -72,7 +224,13 @@ int main (int argc, char *argv[])
   uint32_t BE_MaxAmpduSize = 65535;
   bool shortGuardInterval = false;
   uint32_t chWidth = 20;
-  int simuTime = 20;
+  int simuTime = 1440;
+  int interfLowStart = 0.5;
+  int interfLowStop = 480;
+  int interfMedStart = 480;
+  int interfMedStop = 720;
+  int interfHiStart = 720;
+  int interfHiStop = 1080;
 
   CommandLine cmd;
   cmd.AddValue ("staManager", "PRC Manager of the STA", staManager);
@@ -84,6 +242,12 @@ int main (int argc, char *argv[])
   cmd.AddValue ("BE_MaxAmpduSize", "BE Mac A-MPDU size", BE_MaxAmpduSize);
   cmd.AddValue ("outputFileName", "Output filename", outputFileName);
   cmd.AddValue ("simuTime", "Simulation time in seconds", simuTime);
+  cmd.AddValue ("interfLowStart", "Start time for low interference generator", interfLowStart);
+  cmd.AddValue ("interfLowStart", "Start time for low interference generator", interfLowStop);
+  cmd.AddValue ("interfMedStart", "Start time for medium interference generator", interfMedStart);
+  cmd.AddValue ("interfMedStart", "Start time for medium interference generator", interfMedStop);
+  cmd.AddValue ("interfHiStart", "Start time for high interference generator", interfHiStart);
+  cmd.AddValue ("interfHiStart", "Start time for high interference generator", interfHiStop);
   cmd.Parse (argc, argv);
 
   // Define the APs
@@ -337,7 +501,7 @@ int main (int argc, char *argv[])
   remote2_1.SetTos(32);
   OnOffHelper onoff2_1 ("ns3::UdpSocketFactory",  remote2_1);
   onoff2_1.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff2_1.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff2_1.SetAttribute ("StartTime", TimeValue (Seconds (240)));
   onoff2_1.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff2_1.Install (wifiApNodes.Get (0)));
 
@@ -345,7 +509,7 @@ int main (int argc, char *argv[])
   remote3_1.SetTos(32);
   OnOffHelper onoff3_1 ("ns3::UdpSocketFactory",  remote3_1);
   onoff3_1.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff3_1.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff3_1.SetAttribute ("StartTime", TimeValue (Seconds (360)));
   onoff3_1.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff3_1.Install (wifiApNodes.Get (0)));
 
@@ -353,7 +517,7 @@ int main (int argc, char *argv[])
   remote4_1.SetTos(32);
   OnOffHelper onoff4_1 ("ns3::UdpSocketFactory",  remote4_1);
   onoff4_1.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff4_1.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff4_1.SetAttribute ("StartTime", TimeValue (Seconds (360)));
   onoff4_1.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff4_1.Install (wifiApNodes.Get (0)));
 
@@ -362,32 +526,32 @@ int main (int argc, char *argv[])
   remote4_2.SetTos(64);
   OnOffHelper onoff4_2 ("ns3::UdpSocketFactory",  remote4_2);
   onoff4_2.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff4_2.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
-  onoff4_2.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  onoff4_2.SetAttribute ("StartTime", TimeValue (Seconds (540)));
+  onoff4_2.SetAttribute ("StopTime", TimeValue (Seconds (1260)));
   apps_source.Add(onoff4_2.Install (wifiApNodes.Get (0)));
 
   InetSocketAddress remote5_2 = InetSocketAddress (i.GetAddress (4), port2);
   remote5_2.SetTos(64);
   OnOffHelper onoff5_2 ("ns3::UdpSocketFactory",  remote5_2);
   onoff5_2.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff5_2.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
-  onoff5_2.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  onoff5_2.SetAttribute ("StartTime", TimeValue (Seconds (540)));
+  onoff5_2.SetAttribute ("StopTime", TimeValue (Seconds (1260)));
   apps_source.Add(onoff5_2.Install (wifiApNodes.Get (0)));
 
   InetSocketAddress remote6_2 = InetSocketAddress (i.GetAddress (5), port2);
   remote6_2.SetTos(64);
   OnOffHelper onoff6_2 ("ns3::UdpSocketFactory",  remote6_2);
   onoff6_2.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff6_2.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
-  onoff6_2.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  onoff6_2.SetAttribute ("StartTime", TimeValue (Seconds (540)));
+  onoff6_2.SetAttribute ("StopTime", TimeValue (Seconds (1260)));
   apps_source.Add(onoff6_2.Install (wifiApNodes.Get (0)));
 
   InetSocketAddress remote7_2 = InetSocketAddress (i.GetAddress (6), port2);
   remote7_2.SetTos(64);
   OnOffHelper onoff7_2 ("ns3::UdpSocketFactory",  remote7_2);
   onoff7_2.SetConstantRate (DataRate ("7Mb/s"), 1420);
-  onoff7_2.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
-  onoff7_2.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
+  onoff7_2.SetAttribute ("StartTime", TimeValue (Seconds (540)));
+  onoff7_2.SetAttribute ("StopTime", TimeValue (Seconds (1260)));
   apps_source.Add(onoff7_2.Install (wifiApNodes.Get (0)));
 
   ///-------Slice 3----------------------------------------
@@ -395,7 +559,7 @@ int main (int argc, char *argv[])
   remote7_3.SetTos(96);
   OnOffHelper onoff7_3 ("ns3::UdpSocketFactory",  remote7_3);
   onoff7_3.SetConstantRate (DataRate ("10Mb/s"), 1420);
-  onoff7_3.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff7_3.SetAttribute ("StartTime", TimeValue (Seconds (660)));
   onoff7_3.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff7_3.Install (wifiApNodes.Get (0)));
 
@@ -403,7 +567,7 @@ int main (int argc, char *argv[])
   remote8_3.SetTos(96);
   OnOffHelper onoff8_3 ("ns3::UdpSocketFactory",  remote8_3);
   onoff8_3.SetConstantRate (DataRate ("10Mb/s"), 1420);
-  onoff8_3.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff8_3.SetAttribute ("StartTime", TimeValue (Seconds (660)));
   onoff8_3.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff8_3.Install (wifiApNodes.Get (0)));
 
@@ -411,7 +575,7 @@ int main (int argc, char *argv[])
   remote9_3.SetTos(96);
   OnOffHelper onoff9_3 ("ns3::UdpSocketFactory",  remote9_3);
   onoff9_3.SetConstantRate (DataRate ("10Mb/s"), 1420);
-  onoff9_3.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff9_3.SetAttribute ("StartTime", TimeValue (Seconds (900)));
   onoff9_3.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff9_3.Install (wifiApNodes.Get (0)));
 
@@ -419,7 +583,7 @@ int main (int argc, char *argv[])
   remote10_3.SetTos(96);
   OnOffHelper onoff10_3 ("ns3::UdpSocketFactory", remote10_3);
   onoff10_3.SetConstantRate (DataRate ("10Mb/s"), 1420);
-  onoff10_3.SetAttribute ("StartTime", TimeValue (Seconds (0.5)));
+  onoff10_3.SetAttribute ("StartTime", TimeValue (Seconds (900)));
   onoff10_3.SetAttribute ("StopTime", TimeValue (Seconds (simuTime)));
   apps_source.Add(onoff10_3.Install (wifiApNodes.Get (0)));
 
@@ -429,14 +593,49 @@ int main (int argc, char *argv[])
   apps_sink = sinkInterferer.Install (wifiInterfererSta.Get (0));
 
   InetSocketAddress remote = InetSocketAddress (i.GetAddress (10), port);
-  OnOffHelper onoff ("ns3::UdpSocketFactory", remote);
-  onoff.SetConstantRate (DataRate ("50Mb/s"), 1420);
-  onoff.SetAttribute ("StartTime", TimeValue (Seconds (20)));
-  onoff.SetAttribute ("StopTime", TimeValue (Seconds (50)));
-  onoff.Install (wifiInterfererAp.Get (0));
+  OnOffHelper onoffLow ("ns3::UdpSocketFactory", remote);
+  onoffLow.SetConstantRate (DataRate ("5Mb/s"), 1420);
+  onoffLow.SetAttribute ("StartTime", TimeValue (Seconds (interfLowStart)));
+  onoffLow.SetAttribute ("StopTime", TimeValue (Seconds (interfLowStop)));
+  onoffLow.Install (wifiInterfererAp.Get (0));
+
+  OnOffHelper onoffMed ("ns3::UdpSocketFactory", remote);
+  onoffMed.SetConstantRate (DataRate ("20Mb/s"), 1420);
+  onoffMed.SetAttribute ("StartTime", TimeValue (Seconds (interfMedStart)));
+  onoffMed.SetAttribute ("StopTime", TimeValue (Seconds (interfMedStop)));
+  onoffMed.Install (wifiInterfererAp.Get (0));
+
+  OnOffHelper onoffHi ("ns3::UdpSocketFactory", remote);
+  onoffHi.SetConstantRate (DataRate ("40Mb/s"), 1420);
+  onoffHi.SetAttribute ("StartTime", TimeValue (Seconds (interfHiStart)));
+  onoffHi.SetAttribute ("StopTime", TimeValue (Seconds (interfHiStop)));
+  onoffHi.Install (wifiInterfererAp.Get (0));
+
+  OnOffHelper onoffLow2 ("ns3::UdpSocketFactory", remote);
+  onoffLow2.SetConstantRate (DataRate ("20Mb/s"), 1420);
+  onoffLow2.SetAttribute ("StartTime", TimeValue (Seconds (1080)));
+  onoffLow2.SetAttribute ("StopTime", TimeValue (Seconds (1440)));
+  onoffLow2.Install (wifiInterfererAp.Get (0));
 
   apps_sink.Start (Seconds (0.5));
   apps_sink.Stop (Seconds (simuTime));
+
+  //------------------------------------------------------------
+  //-- Setup stats and data collection
+  //--------------------------------------------
+
+  //Statistics counters
+  NodeStatistics statisticsAp0 = NodeStatistics (wifiApDevices, wifiStaDevices);
+
+  //Register packet receptions to calculate throughput
+  //Config::Connect ("/NodeList/2/ApplicationList/*/$ns3::PacketSink/Rx",
+    //               MakeCallback (&NodeStatistics::RxCallback, &statisticsAp0));
+
+  //Register States
+  Config::Connect ("/NodeList/0/DeviceList/*/$ns3::WifiNetDevice/Phy/$ns3::YansWifiPhy/State/State",
+                   MakeCallback (&NodeStatistics::StateCallback, &statisticsAp0));
+
+  statisticsAp0.CheckStatistics (1);
 
   // Calculate Throughput using Flowmonitor
   //
@@ -455,6 +654,46 @@ int main (int argc, char *argv[])
       NS_LOG_UNCOND ("Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n");
       NS_LOG_UNCOND ("  Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds () - i->second.timeFirstTxPacket.GetSeconds ()) / 1024 / 1024  << " Mbps\n");
     }
+
+  std::ofstream outfileTh0 (("throughput-" + outputFileName + "-0.plt").c_str ());
+  Gnuplot gnuplot = Gnuplot (("throughput-" + outputFileName + "-0.eps").c_str (), "Throughput");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Throughput (Mb/s)");
+  gnuplot.SetTitle ("Throughput (AP0 to STA) vs time");
+  gnuplot.AddDataset (statisticsAp0.GetDatafile ());
+  gnuplot.GenerateOutput (outfileTh0);
+
+  std::ofstream outfileTx0 (("tx-" + outputFileName + "-0.plt").c_str ());
+  gnuplot = Gnuplot (("tx-" + outputFileName + "-0.eps").c_str (), "Time in TX State");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Percent");
+  gnuplot.SetTitle ("Percentage time AP0 in TX state vs time");
+  gnuplot.AddDataset (statisticsAp0.GetTxDatafile ());
+  gnuplot.GenerateOutput (outfileTx0);
+
+  std::ofstream outfileRx0 (("rx-" + outputFileName + "-0.plt").c_str ());
+  gnuplot = Gnuplot (("rx-" + outputFileName + "-0.eps").c_str (), "Time in RX State");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Percent");
+  gnuplot.SetTitle ("Percentage time AP0 in RX state vs time");
+  gnuplot.AddDataset (statisticsAp0.GetRxDatafile ());
+  gnuplot.GenerateOutput (outfileRx0);
+
+  std::ofstream outfileBusy0 (("busy-" + outputFileName + "-0.plt").c_str ());
+  gnuplot = Gnuplot (("busy-" + outputFileName + "-0.eps").c_str (), "Time in Busy State");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Percent");
+  gnuplot.SetTitle ("Percentage time AP0 in Busy state vs time");
+  gnuplot.AddDataset (statisticsAp0.GetBusyDatafile ());
+  gnuplot.GenerateOutput (outfileBusy0);
+
+  std::ofstream outfileIdle0 (("idle-" + outputFileName + "-0.plt").c_str ());
+  gnuplot = Gnuplot (("idle-" + outputFileName + "-0.eps").c_str (), "Time in Idle State");
+  gnuplot.SetTerminal ("post eps color enhanced");
+  gnuplot.SetLegend ("Time (seconds)", "Percent");
+  gnuplot.SetTitle ("Percentage time AP0 in Idle state vs time");
+  gnuplot.AddDataset (statisticsAp0.GetIdleDatafile ());
+  gnuplot.GenerateOutput (outfileIdle0);
 
   Simulator::Destroy ();
 
